2306ea5d32492d392da7d42b166543bfffff43f3
Prerna Chavan <prerna.chavan@bakerhughes.com>
2025-01-10 11:14:48 +0530

    Instead of a plain ID UUID is used here

---
+++ models/alerts.js
@@ -0,0 +1,34 @@
+const { DataTypes} = require('sequelize');
+const sequelize = require('../config/db');
+const { v4: uuidv4 } = require('uuid');
+
+const Alerts = sequelize.define('alerts',{
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: uuidv4,
+        primaryKey: true,
+        autoIncrement: false,
+    },
+    type_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: 'types',
+            key: 'id'
+        }
+    },
+    payload: {
+        type: DataTypes.JSONB,
+        allowNull: false,
+    },
+    
+}, 
+{
+    tableName: 'alerts',
+    schema: 'public',
+    timestamps: true,
+});
+
+
+// console.log(Type);
+module.exports = Alerts;
